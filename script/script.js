3619b6a03334c46d6bfb7ff329b33c0970a7f1ce
annaodz <99annaj@gmail.com>
2020-01-30 00:24:49 +0100

    second page

--- script/script.js
+++ script/script.js
@@ -1,7 +1,18 @@
 
-      var mdate = document.getElementById("date");
-      mdate.onclick = function () {
-        document.getElementById('demo').innerHTML = new Date();
-      }
-      var d = new Date();
-document.getElementById("demo").innerHTML = d.getDate();
+       document.getElementById("demo").addEventListener("click", myFunction);
+
+       function myFunction() {
+         
+       // To set two dates to two variables 
+       var date1 = new Date("2019-09-01"); 
+       var today = new Date(); 
+         
+       // To calculate the time difference of two dates 
+       var date2= today.getTime() - date1.getTime(); 
+         
+       // To calculate the no. of days between two dates 
+       var date2 = date2 / (1000 * 3600 * 24); 
+        
+       //To display the final no. of days (result) 
+         document.getElementById("demo").innerHTML = "Hi! I'm Ann and this is my <br>"+ Math.round(date2) +"<br>day of learning JS";
+       }
